21c89ed19292e7a0777298a46b3673c93b3329ed
DevDarl <darlingtonokoriec@gmail.com>
2021-07-16 21:35:43 +0100

    changes

--- auth.php
+++ auth.php
@@ -23,7 +23,7 @@ if (isset($_SESSION['success'])) {
             <div class="panel panel-info" >
                     <div class="panel-heading">
                         <div class="panel-title">Sign In To Upload Products</div>
-                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
+                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="password.php">Forgot password?</a></div>
                     </div>     
 
                     <div style="padding-top:30px" class="panel-body" >
